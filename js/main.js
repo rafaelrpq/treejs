73c87be640320903e9bf45893a84797536feba6e
Rafael Quevedo <rafaelrpq@gmail.com>
2025-06-18 13:50:20 -0300

    fix camera near plane and enable pixel ratio for better rendering

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@ const scene = new THREE.Scene()
 const camera = new THREE.PerspectiveCamera(
     75,
     window.innerWidth / window.innerHeight,
-    0.1,
+    1,
     1000
 )
 camera.position.set (0,16,16)
@@ -34,7 +34,8 @@ scene.add(light)
 renderer.setSize(window.innerWidth, window.innerHeight)
 renderer.shadowMap.enabled = true
 // renderer.shadowMap.type = THREE.PCFSoftShadowMap
-// renderer.setPixelRatio(window.devicePixelRatio)
+renderer.setPixelRatio(window.devicePixelRatio)
+renderer.setClearColor(0x000000, 0)
 
 
 const controls = new OrbitControls( camera, renderer.domElement );
